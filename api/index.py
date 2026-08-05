b1e1be708c1f290f1253280f82a45c3bd9d85a58
mounirammari312 <mounirammari550@gmail.com>
2026-08-05 21:33:34 +0100

    Remove 'akwam' configuration from index.py
    Removed the 'akwam' configuration from the API index.

--- api/index.py
+++ api/index.py
@@ -174,17 +174,6 @@ def get_config():
                 'requires_unpack': True,
                 'ajax_required': True,
             },
-            {
-                'name': 'akwam',
-                'domain': AKWAM_BASE_DOMAIN,
-                'search_path': '/search?q={query}',
-                'movie_selector': 'a[href*=/movie/]',
-                'series_selector': 'a[href*=/series/]',
-                'ep_selector': 'a[href*=/episode/]',
-                'watch_selector': 'a[href*=/watch/], a.link-btn',
-                'link_regex': r'https?://[^\s"\'<>]+\.(?:mp4)[^\s"\'<>]*',
-                'requires_unpack': False,
-            },
             
             {
                 'name': 'moviz-time',
