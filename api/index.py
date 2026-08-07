6ccdf9f069657739504b4d2d0d0780d3454f1d2b
mounirammari312 <mounirammari550@gmail.com>
2026-08-07 18:32:26 +0100

    Update selectors and unpack requirement in index.py

--- api/index.py
+++ api/index.py
@@ -169,18 +169,20 @@ def get_config():
                     
                 
                 
+                
         'name': 'royal-drama',
         'domain': 'https://w9.royal-drama.com',
         'search_path': '/?s={query}',
-        'card_selector': 'a[href*="view.php?vid="]',
-        'watch_selector': 'iframe, [data-link], [data-url], [data-post]',
-        'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url]',
+        'card_selector': 'a[href*="view.php"]',
+        'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
+        'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
         'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
-        'requires_unpack': False,
+        'requires_unpack': True,
         'ajax_required': False,
-        'movie_selector': 'a[href*="view.php?vid="]',
-        'series_selector': 'a[href*="view.php?vid="]'
+        'movie_selector': 'a[href*="view.php"]',
+        'series_selector': 'a[href*="view.php"]'
         
+
 
 
 
