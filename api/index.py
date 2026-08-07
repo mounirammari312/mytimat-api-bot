8501bbd2968abca51fcac3e89b0a23575f6b2076
mounirammari312 <mounirammari550@gmail.com>
2026-08-07 11:13:37 +0100

    Add new provider 'moviz-time' with selectors

--- api/index.py
+++ api/index.py
@@ -167,16 +167,19 @@ def get_config():
         'providers': [
  {
     
+    
     'name': 'moviz-time',
     'domain': 'https://moviz-time.site',
     'search_path': '/?s={query}',
     'card_selector': 'a[href*="/watch/"], a[href*="/series/"], article.post a',
-    'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab',
-    'iframe_selector': 'iframe, iframe[data-src]',
+    'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
+    'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
     'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
     'requires_unpack': True,
     'ajax_required': True,
     'series_selector': 'a[href*="/series/"]'
+
+
 
 
 },
