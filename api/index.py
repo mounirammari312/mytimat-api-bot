5e1bf9d85e24d816fc5a656bf99978a372106088
mounirammari312 <mounirammari550@gmail.com>
2026-08-07 14:29:27 +0100

    Replace 'starcima' with 'royal-drama' provider

--- api/index.py
+++ api/index.py
@@ -167,17 +167,15 @@ def get_config():
             {
                         
             
-        'name': 'starcima',
-        'domain': 'https://starcima.cc',
+        'name': 'royal-drama',
+        'domain': 'https://w9.royal-drama.com',
         'search_path': '/?s={query}',
         'card_selector': 'a[href*="/watch/"], a[href*="/series/"]',
         'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
         'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
-        'link_regex': 'https?://[^\\s"\\\'<>]+\\.(?:m3u8|mp4|txt)[^\\s"\\\'<>]*',
+        'link_regex': 'https?://[^\\s"\\\'<>]+\\.(?:m3u8|mp4|txt)[^\\\\s"\\\'<>]*',
         'requires_unpack': True,
         'ajax_required': True,
-        'movie_selector': 'a[href*="/watch/"]',
-        'series_selector': 'a[href*="/series/"]'
         
 
             },
