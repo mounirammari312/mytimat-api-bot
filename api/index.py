175bdcde5bd9a5d20d52524493e03fff405b802b
mounirammari312 <mounirammari550@gmail.com>
2026-08-07 19:01:42 +0100

    Replace royal-drama configuration with qfilm

--- api/index.py
+++ api/index.py
@@ -170,18 +170,14 @@ def get_config():
                 
                 
                 
-        'name': 'royal-drama',
-        'domain': 'https://w9.royal-drama.com',
-        'search_path': '/?s={query}',
-        'card_selector': 'a[href*="view.php"]',
-        'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
-        'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
-        'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
-        'requires_unpack': True,
-        'ajax_required': False,
-        'movie_selector': 'a[href*="view.php"]',
-        'series_selector': 'a[href*="view.php"]'
-        
+     'name': 'qfilm',
+    'domain': 'https://a.qfilm.tv',
+    'search_path': '/?s={query}',
+    'card_selector': 'a[href*="/watch/"], a[href*="/play.php"], a[href*="vid="]',
+    'watch_selector': 'iframe, [data-link], [data-url], [data-post], a[href*="watch.php"], a[href*="play.php"]',
+    'link_regex': r'https?://[^\s"'<>]+\.(?:m3u8|mp4|txt)[^\s"'<>]*',
+    'requires_unpack': True,
+    'ajax_required': False
 
 
 
