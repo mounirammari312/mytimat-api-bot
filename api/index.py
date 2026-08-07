1d2c0de7a453c7c35edc9fa43156b3132ac9b372
mounirammari312 <mounirammari550@gmail.com>
2026-08-07 10:08:13 +0100

    Add new provider 'moviz-time' to the API

--- api/index.py
+++ api/index.py
@@ -166,16 +166,18 @@ def get_config():
         'version': '6.3.0',
         'providers': [
             {
-                'name': 'akwam',
-                'domain': AKWAM_BASE_DOMAIN,
-                'search_path': '/search?q={query}',
-                'movie_selector': 'a[href*=/movie/]',
-                'series_selector': 'a[href*=/series/]',
-                'ep_selector': 'a[href*=/episode/]',
-                'watch_selector': 'a[href*=/watch/], a.link-btn',
-                'link_regex': r'https?://[^\s"\'<>]+\.(?:mp4)[^\s"\'<>]*',
-                'requires_unpack': False,
-            },
+    'name': 'moviz-time',
+    'domain': 'https://moviz-time.site',
+    'search_path': '/?s={query}',
+    'card_selector': 'a[href*="/series/"], article.post a',
+    'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab',
+    'iframe_selector': 'iframe, iframe[data-src]',
+    'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
+    'requires_unpack': True,
+    'ajax_required': True,
+    'series_selector': 'a[href*="/series/"]'
+},
+
             {
                 'name': 'larroza',
                 'domain': LARROZA_BASE_DOMAIN,
@@ -184,7 +186,7 @@ def get_config():
                 'iframe_selector': 'iframe',
                 'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4)[^\s"\'<>]*',
                 'requires_unpack': True,
-            },,
+            },
 
 
 
