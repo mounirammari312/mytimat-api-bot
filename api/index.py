f50e334807bf1c71edf1e9e072e7fbc4ef0314c6
mounirammari312 <mounirammari550@gmail.com>
2026-08-08 09:07:18 +0100

    Add new provider 'qfilm' to the API

--- api/index.py
+++ api/index.py
@@ -167,16 +167,17 @@ def get_config():
         'providers': [
             
             {
-                'name': 'moviz-time',
-                'domain': 'https://moviz-time.site',
-                'search_path': '/?s={query}',
-                'card_selector': 'a[href*="/watch/"], a[href*="/series/"], article.post a',
-                'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
-                'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
-                'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
-                'requires_unpack': True,
-                'ajax_required': True,
-                'series_selector': 'a[href*="/series/"]'
+                
+    'name': 'qfilm',
+    'domain': 'https://a.qfilm.tv',
+    'search_path': '/search.php?keywords={query}',
+    'card_selector': 'a[href*="watch.php"]',
+    'watch_selector': 'a[href*="play.php"], a[href*="watch.php"], a[href*="embed.php"], iframe, [data-url]',
+    'iframe_selector': 'iframe',
+    'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
+    'requires_unpack': False,
+    'ajax_required': False
+
             },
         ],
     })
