37c5f9c11701be68d899209585545a1848167c7c
mounirammari312 <mounirammari550@gmail.com>
2026-08-08 10:55:11 +0100

    Add selectors for series and episode links

--- api/index.py
+++ api/index.py
@@ -169,13 +169,18 @@ def get_config():
             {
                 
                
+            
+
+
             "name": "qfilm",
             "domain": "https://a.qfilm.tv",
             "search_path": "/?s={query}",
             "card_selector": "a[href*=\"/watch/\"], a[href*=\"/play.php\"], a[href*=\"vid=\"], article a, .item a, .result-item a",
+            "series_selector": "a[href*=\"/series/\"], a[href*=\"series\"], article a, .item a",
+            "ep_selector": "a[href*=\"/episode/\"], a[href*=\"episode\"], a[href*=\"ep\"], a[href*=\"watch.php\"], .episodelist a, .episodes a",
             "watch_selector": "iframe, [data-link], [data-url], [data-post], a[href*=\"watch.php\"], a[href*=\"play.php\"], .single_tab, .play-btn, .server-item",
             "iframe_selector": "iframe, iframe[data-src], [data-src], [data-url], [data-link]",
-            "link_regex": r"https?://[^\s\"'<>]+\.(?:m3u8|mp4|txt)[^\s\"<>]*",
+            "link_regex": r"https?://[^\s\"\'<>]+\.(?:m3u8|mp4|txt)[^\s\"<>]*",
             "requires_unpack": True,
             "ajax_required": True
         
