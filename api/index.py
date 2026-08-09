61046e2407e1e31e075e0d29faef884142dd02bc
mounirammari312 <mounirammari550@gmail.com>
2026-08-10 00:25:24 +0100

    Remove unused provider configurations
    Removed unused provider configurations for 'akwam' and 'moviz-time'.

--- api/index.py
+++ api/index.py
@@ -189,17 +189,7 @@ def get_config():
         'status': 'success',
         'version': '6.3.0',
         'providers': [
-            {
-                'name': 'akwam',
-                'domain': AKWAM_BASE_DOMAIN,
-                'search_path': '/search?q={query}',
-                'movie_selector': 'a[href*=/movie/]',
-                'series_selector': 'a[href*=/series/]',
-                'ep_selector': 'a[href*=/episode/]',
-                'watch_selector': 'a[href*=/watch/], a.link-btn',
-                'link_regex': r'https?://[^\s"\'<>]+\.(?:mp4)[^\s"\'<>]*',
-                'requires_unpack': False,
-            },
+            
             {
                 'name': 'larroza',
                 'domain': LARROZA_BASE_DOMAIN,
@@ -208,18 +198,6 @@ def get_config():
                 'iframe_selector': 'iframe',
                 'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4)[^\s"\'<>]*',
                 'requires_unpack': True,
-            },
-            {
-                'name': 'moviz-time',
-                'domain': 'https://moviz-time.site',
-                'search_path': '/?s={query}',
-                'card_selector': 'a[href*="/watch/"], a[href*="/series/"], article.post a',
-                'watch_selector': 'iframe, [data-link], [data-url], [data-post], .single_tab, .play-btn, .server-item',
-                'iframe_selector': 'iframe, iframe[data-src], [data-src], [data-url], [data-link]',
-                'link_regex': r'https?://[^\s"\'<>]+\.(?:m3u8|mp4|txt)[^\s"\'<>]*',
-                'requires_unpack': True,
-                'ajax_required': True,
-                'series_selector': 'a[href*="/series/"]'
             },
         ],
     })
